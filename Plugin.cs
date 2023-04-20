d16d4ffb6e3379ef157c69c90a2da291a604ca4b
Drahsid <Drahsid@gmail.com>
2023-04-20 18:43:14 -0400

    Bump version

--- Plugin.cs
+++ Plugin.cs
@@ -10,7 +10,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
-[assembly: System.Reflection.AssemblyVersion("1.1.3")]
+[assembly: System.Reflection.AssemblyVersion("1.1.4")]
 
 namespace BetterShadows;
 
