e6a618139d30d7bd9b83bcfa5b81242b62151a29
Drahsid <Drahsid@gmail.com>
2024-07-03 05:07:25 -0400

    Bump version

--- BetterShadows/AssemblyAttributes.cs
+++ BetterShadows/AssemblyAttributes.cs
@@ -4,7 +4,7 @@
 [assembly: System.Reflection.AssemblyProduct("BetterShadows")]
 [assembly: System.Reflection.AssemblyTitle("BetterShadows")]
 
-[assembly: System.Reflection.AssemblyVersion("1.4.0")]
+[assembly: System.Reflection.AssemblyVersion("1.4.1")]
 
 #if DEBUG
 [assembly: System.Reflection.AssemblyConfiguration("Debug")]
